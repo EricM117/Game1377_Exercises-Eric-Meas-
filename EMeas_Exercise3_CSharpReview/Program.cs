0a373d216e784e2debe983f05210e01d0a9bca33
EricM117 <ericmeas@hotmail.com>
2025-02-01 18:26:56 -0600

    New Project for next assignment

---
+++ EMeas_Exercise3_CSharpReview/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EMeas_Exercise3_CSharpReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence01 = "I’m in Game 1377: Scripting for Game Designers.";
+            string sUpper = sentence01.ToUpper();
+            string sLower = sentence01.ToLower();
+            string sPartialWord01 = sentence01.Substring(8);
+            string sPartialWord02 = sentence01.Substring(8, 11);
+            Console.WriteLine(sentence01);
+            Console.WriteLine(sUpper);
+            Console.WriteLine(sLower);
+            Console.WriteLine(sPartialWord01);
+            Console.WriteLine(sPartialWord02);
+        }
+    }
+}
